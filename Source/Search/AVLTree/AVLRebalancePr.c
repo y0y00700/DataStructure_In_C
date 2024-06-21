afd7de37571d14b889aed640103b85b9ec53edd4
Min <57902790+y0y00700@users.noreply.github.com>
2024-06-21 18:17:13 +0900

    AVL Tree Type별 회전 및 Balance Function 구현 실습
    commit

---
+++ Source/Search/AVLTree/AVLRebalancePr.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include "BinarySearchTree3.h"
+
+BTreeNode * RotateLL(BTreeNode * bst){
+
+    BTreeNode * pNode;
+    BTreeNode * cNode;
+    pNode = bst;
+    cNode = GetLeftSubTree(bst);
+
+    ChangeLeftSubTree(pNode,GetRightSubTree(cNode));
+    ChangeRightSubTree(cNode,pNode);
+    return cNode;
+}
+
+BTreeNode * RotateRR(BTreeNode * bst){
+    BTreeNode * pNode;
+    BTreeNode * cNode;
+    pNode = bst;
+    cNode = GetRightSubTree(pNode);
+
+    ChangeRightSubTree(pNode,GetLeftSubTree(cNode));
+    ChangeLeftSubTree(cNode,pNode);
+    return cNode;
+}
+
+BTreeNode * RotateLR(BTreeNode * bst){
+    BTreeNode * pNode;
+    BTreeNode * cNode;
+
+    pNode = bst;
+    cNode = GetLeftSubTree(bst);
+
+    ChangeLeftSubTree(pNode,RotateRR(cNode));
+    return RotateLL(pNode);
+}
+
+BTreeNode * RotateRL(BTreeNode * bst){
+    BTreeNode * pNode;
+    BTreeNode * cNode;
+
+    pNode = bst;
+    cNode = GetRightSubTree(pNode);
+
+    ChangeLeftSubTree(pNode,RotateLL(cNode));   
+    return RotateRR;
+}
+
+int GetHeight(BTreeNode * bst){
+
+    int leftH;
+    int rightH;
+
+    if(bst == NULL)
+        return 0;
+    leftH = GetHeight(bst->left);
+    rightH = GetHeight(bst->right);
+
+    if(leftH>rightH)
+        return leftH+1;
+    else
+        return rightH+1;
+}
+
+int GetHeightDiff(BTreeNode * bst){
+    int lsh;
+    int rsh;
+
+    if(bst == NULL)
+        return 0;
+    lsh = GetHeight(GetLeftSubTree(bst));
+    rsh = GetHeight(GetRightSubTree(bst));
+
+    return lsh-rsh;
+}
+
+BTreeNode * Rebalance(BTreeNode ** pRoot){
+    int hDiff = GetHeightDiff(*pRoot);
+    if(hDiff > 1){
+        if(GetHeightDiff(GetLeftSubTree(*pRoot))>0)
+            *pRoot = RotateLL(*pRoot);
+        else
+            *pRoot = RotateLR(*pRoot);
+    }
+    if(hDiff < -1){
+        if(GetHeightDiff(GetRightSubTree(*pRoot)) < 0)
+            *pRoot = RotateRR(*pRoot);
+        else
+            *pRoot = RotateRL(*pRoot);
+    }
+
+    return *pRoot;
+}
