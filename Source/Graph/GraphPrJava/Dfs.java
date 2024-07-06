64faf4fcc28ae6d9904b3d98f9ba0a5118f0febb
Min <57902790+y0y00700@users.noreply.github.com>
2024-07-06 22:14:34 +0900

    DFS 동작 순서 만들기 및 BFS 동작 순서 및 구현 학습
    commit;

--- Source/Graph/GraphPrJava/Dfs.java
+++ Source/Graph/GraphPrJava/Dfs.java
@@ -6,11 +6,10 @@ import java.util.Stack;
 
 /*  pseudoCode
     // 깊이 우선 탐색의 구현
-    void search(Node root){
-        // 1. root 노드 방문
-        visit(root);
-        root.visited = true; // 1-1. 방문한 노드를 표시
-        // 2. root 노드와 인접한 정점을 모두 방문
+    void search(Graph vertex){
+        visit(vertex); // 1. 기준 vertex 방문
+        vertex.visited = true; // 1-1. 방문한 노드를 표시
+        // 2. 현재 정점과 인접한 정점을 모두 방문
         for each(Node n in root.adjacent){
             if(n.visited == false){ // 4. 방문하지 않은 정점을 찾는다.
                 search(n); // 3. root 노드와 인접한 정점 정점을 시작 정점으로 DFS 시작
@@ -19,44 +18,58 @@ import java.util.Stack;
     }
  */
 
+// void DFSWithStack (Graph vertex){
+//     let S be a stack;  // 1. 스택을 초기화
+//     S.push(vertex);    // 2. 시작 정점을 stack에 push
+//     while S is not empty do{ // 3. Stack이 Empty 되면 반복 종료
+//         v = S.pop() // 3. 최초 시작 정점을 Stack에 저장
+//         if (vertex is not labeled as visited then){  // 4. 방문되지 않은 정점인 경우,
+//             label v as discovered // 5. 정점을 방문 처리
+//             for (all edges from v to w in Graph.adjacentEdges(v) do){ // 6. 해당 정점을 기준으로 인접 정점을 차례대로 방문
+//                 if (w is not labeled as visited then) // 7. 아직 방문되지 않은 정점으로 이동시, Stack에 현재 정점을 저장
+//                   S.push(w)
+//             }
+//         }
+//     }
+// }
  class Graph{
     private int V;
-    private LinkedList<Integer> adj[];
+    private LinkedList<Integer> adjList[];
    
     Graph(int v){
         V = v;
-        adj = new LinkedList[v];
+        adjList = new LinkedList[v];
         for(int i=0;i<v;i++)
-            adj[i] = new LinkedList();
+        adjList[i] = new LinkedList();
     }
 
     void addEdge(int v,int w) {
-        adj[v].add(w);
+        adjList[v].add(w);
     }
 
-    void DFSUtil(int v,boolean visited[]){
+    void DFSHelper(int v,boolean visited[]){
         visited[v] = true;
         System.out.print(v + " ");
 
-        Iterator <Integer> i = adj[v].listIterator();
+        Iterator <Integer> i = adjList[v].listIterator();
         while(i.hasNext()){
             int n = i.next();
 
             if(!visited[n])
-                DFSUtil(n, visited);
+                DFSHelper(n, visited);
         }
     }
 
     void DFS(int v){
         boolean visited[] = new boolean[V];
-        DFSUtil(v, visited);
+        DFSHelper(v, visited);
         System.out.println(" ");
     }
 
     void DFS(){
         boolean visited[] = new boolean[V];
         for(int i=0;i<V;i++){
-            if(visited[i]== false)  DFSUtil(i, visited);
+            if(visited[i]== false)  DFSHelper(i, visited);
         }
     }
 
@@ -85,7 +98,7 @@ import java.util.Stack;
                 System.out.print(nextV+" ");
                 visited[nextV] = true;
                 for(int i=0;i<adj[nextV].size();i++){
-                        int nV = adj[nextV].get(i);
+                        int nV = adjList[nextV].get(i);
                         if(!visited[i])
                             stV.push(i);
                 }
@@ -105,11 +118,9 @@ public class Dfs {
         g.addEdge(1, 2);
         g.addEdge(2, 0);
         g.addEdge(2, 3);
-        g.addEdge(3, 3);
 
         g.DFS(2);
         g.DFS();
-        //System.out.println(" ");
         //g.DFSWithStack(2);
     }
    
